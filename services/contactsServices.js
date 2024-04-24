36f3ea599a359725942397b756808f72e6bd3878
AnnaFedorovaGit <ann.petrovska94@gmail.com>
2024-04-24 14:14:24 +0300

    fix notice

--- services/contactsServices.js
+++ services/contactsServices.js
@@ -4,23 +4,23 @@ import Contact from "../models/Contact.js";
 export const listContacts = (filter ={}, setting = {}) => Contact.find(filter, "-createdAt -updatedAt", setting).populate("owner", "username email")
 
 
-export const getContactById = async (contactId) => {
-    const contact = await Contact.findById(contactId);
+export const getContactById = async (filter) => {
+    const contact = await Contact.findOne(filter);
     return contact || null;
 }
 
-export const removeContact = (contactId) => {
-    return Contact.findByIdAndDelete(contactId);
+export const removeContact = (filter) => {
+    return Contact.findOneAndDelete(filter);
 }
 
 export const addContact = ({ name, email, phone, owner }) => { 
     return Contact.create({ name, email, phone, owner });
 }
 
-export const updateContact = (contactId, body) => {
-    return Contact.findByIdAndUpdate(contactId, body);
+export const updateContact = (filter, body) => {
+    return Contact.findOneAndUpdate(filter, body);
 }
 
-export const updateStatusContact = (contactId, body) => {
-    return Contact.findByIdAndUpdate(contactId, body);
+export const updateStatusContact = (filter, body) => {
+    return Contact.findByIdAndUpdate(filter, body);
 }
